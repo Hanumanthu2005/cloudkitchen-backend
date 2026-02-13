e19be368ccb941797fa53048e5c5f86f9fa61788
Hanumanthu2005 <hanumanthurao9381@gmail.com>
2026-02-13 09:49:22 +0530

    init commit

---
+++ src/main/java/com/hanu/cloudkitchen/CloudkitchenApplication.java
@@ -0,0 +1,13 @@
+package com.hanu.cloudkitchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudkitchenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudkitchenApplication.class, args);
+	}
+
+}
